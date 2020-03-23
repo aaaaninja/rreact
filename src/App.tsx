f70e1585bd11f0d6c0230dc387d926b47e0d9522
aaaaninja <aaaaninja@aaaa.ninja>
2020-03-23 15:23:22 +0900

    import文を絶対パスでかけるように

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from 'logo.svg';
+import 'App.css';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.<br />
-          こんにちはこんにちは！！
+          こんにちはこんにちは！！hoge
         </p>
         <a
           className="App-link"
