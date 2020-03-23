d960983dfbcccb073f79cc0123415c185aa82a12
aaaaninja <aaaaninja@aaaa.ninja>
2020-03-23 19:15:07 +0900

    :tada: その結果

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,24 @@ import React from 'react';
 import logo from 'logo.svg';
 import 'App.css';
 
-export default function () {
+export default function() {
   const opts = {
-    className: "App-link",
-    href: "https://reactjs.org",
-    target: "_blank",
-    rel: "noopener noreferrer"
-  }
+    className: 'App-link',
+    href: 'https://reactjs.org',
+    target: '_blank',
+    rel: 'noopener noreferrer',
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.<br />
+          Edit <code>src/App.tsx</code> and save to reload.
+          <br />
           こんにちはこんにちは！！hoge
         </p>
-        <a {...opts}>
-          Learn React
-        </a>
+        <a {...opts}>Learn React</a>
       </header>
     </div>
   );
